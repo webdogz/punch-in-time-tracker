38262f36451b4b78fe9dd38fcf3577711d5f7c61
webdogz <vernon.laskey@gmail.com>
2014-08-21 00:45:39 +0800

    Add ImageBrush, width, height to ImageButton and tidy up modern theme by removing extra padding, margins. Fix theme on watermark controls

--- PunchIn/Controls/ImageButton.cs
+++ PunchIn/Controls/ImageButton.cs
@@ -11,19 +11,50 @@ namespace PunchIn.Controls
 {
     public class ImageButton : Button
     {
-        static ImageButton()
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageButton), new UIPropertyMetadata(null));
+        public static readonly DependencyProperty ImageBrushProperty =
+            DependencyProperty.Register("ImageBrush", typeof(Brush), typeof(ImageButton), new UIPropertyMetadata(null));
+        public static readonly DependencyProperty ImageWidthProperty =
+            DependencyProperty.Register("ImageWidth", typeof(double), typeof(ImageButton), new UIPropertyMetadata(20D));
+        public static readonly DependencyProperty ImageHeightProperty =
+            DependencyProperty.Register("ImageHeight", typeof(double), typeof(ImageButton), new UIPropertyMetadata(20D));
+        
+        public ImageButton()
         {
-            //set DefaultStyleKeyProperty
+            this.DefaultStyleKey = typeof(ImageButton);
         }
-
+        /// <summary>
+        /// Gets or sets the Image
+        /// </summary>
         public ImageSource ImageSource
         {
             get { return (ImageSource)GetValue(ImageSourceProperty); }
             set { SetValue(ImageSourceProperty, value); }
         }
-
-        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ImageSourceProperty =
-            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(ImageButton), new UIPropertyMetadata(null));
+        /// <summary>
+        /// Gets or sets the Brush
+        /// </summary>
+        public Brush ImageBrush
+        {
+            get { return (Brush)GetValue(ImageBrushProperty); }
+            set { SetValue(ImageBrushProperty, value); }
+        }
+        /// <summary>
+        /// Gets or sets the width of the Image and Brush
+        /// </summary>
+        public double ImageWidth
+        {
+            get { return (double)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+        /// <summary>
+        /// Gets or sets the height of the Image and Brush
+        /// </summary>
+        public double ImageHeight
+        {
+            get { return (double)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
     }
 }
