07b63228c28568cd53720972770efe5bb03d7d5f
webdogz <vernon.laskey@gmail.com>
2014-09-01 02:58:17 +0800

    Add filtering for Reports. Add error content for reports and modify error text foreground so you can read it against the error contents background

---
+++ PunchIn/Controls/BindableRadioButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PunchIn.Controls
+{
+    public class BindableRadioButton : RadioButton
+    {
+        protected override void OnChecked(RoutedEventArgs e)
+        {
+            // Do nothing. This will prevent IsChecked from being manually set and overwriting the binding.
+        }
+
+        protected override void OnToggle()
+        {
+            // Do nothing. This will prevent IsChecked from being manually set and overwriting the binding.
+        }
+    }
+}
--- PunchIn/ViewModels/ReportsWeeklyViewModel.cs
+++ PunchIn/ViewModels/ReportsWeeklyViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using PunchIn.Models;
 using PunchIn.Services;
 using System;
+using System.Linq;
 using System.Collections.ObjectModel;
 using PunchIn.Extensions;
+using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace PunchIn.ViewModels
 {
@@ -12,27 +15,91 @@ namespace PunchIn.ViewModels
         public ReportsWeeklyViewModel()
         {
             this.service = new PunchInService();
-            int thisWeek = DateTime.Now.GetWeekOfYear();
-            ReportItems = new ObservableCollection<ReportExportItem>(this.service.GetReportExportItems());
+            reportList = this.service.GetReportExportItems();
+            try
+            {
+                int week = DateTime.Now.GetWeekOfYear();
+                WeekOfYearFilter = WeekOfYearList.LastOrDefault().WeekOfYear;
+            }
+            catch (Exception ex)
+            {
+                Errors = ex.Message;
+            }
         }
-        public string MyIcon { get { return "backlogitem";} }
-        private int weekOfYear;
-        public int WeekOfYear
+        #region Filter Propterties
+        private ObservableCollection<WorkTypesListCheckbox> workTypesFilter;
+        public ObservableCollection<WorkTypesListCheckbox> WorkTypesFilter
         {
-            get { return this.weekOfYear; }
+            get 
+            {
+                if (this.workTypesFilter == null)
+                    this.workTypesFilter = new ObservableCollection<WorkTypesListCheckbox>(
+                        Enum.GetValues(typeof(WorkTypes)).Cast<WorkTypes>()
+                        .Select(wt => new WorkTypesListCheckbox
+                        {
+                            WorkType = wt,
+                            Text = wt.ToString(),
+                            IsSelected = true,
+                            Command = CheckedCommand
+                        }));
+                return this.workTypesFilter; 
+            }
             set
             {
-                if (this.weekOfYear != value)
+                if (this.workTypesFilter != value)
                 {
-                    this.weekOfYear = value;
-                    OnPropertyChanged("WeekOfYear");
+                    this.workTypesFilter = value;
+                    OnPropertyChanged("WorkTypesFilter");
+                    OnPropertyChanged("ReportItems");
                 }
             }
         }
+        private List<WeekOfYearItem> weekOfYearList;
+        public List<WeekOfYearItem> WeekOfYearList
+        {
+            get
+            {
+                if (weekOfYearList == null)
+                {
+                    weekOfYearList = reportList.Select(r => r.WeekOfYear)
+                        .Distinct().Select(d => new WeekOfYearItem
+                        {
+                            WeekOfYear = d,
+                            WeekOfYearDate = d.GetWeekOfYearDate().ToString("dd/MM/yyyy")
+                        }).ToList();
+                }
+                return weekOfYearList;
+            }
+        }
+        private int weekOfYearFilter;
+        public int WeekOfYearFilter
+        {
+            get { return this.weekOfYearFilter; }
+            set
+            {
+                if (this.weekOfYearFilter != value)
+                {
+                    this.weekOfYearFilter = value;
+                    OnPropertyChanged("WeekOfYearFilter");
+                    OnPropertyChanged("ReportItems");
+                }
+            }
+        }
+        #endregion
+
+        #region List Properties
+        private readonly List<ReportExportItem> reportList;
         private ObservableCollection<ReportExportItem> reportItems;
         public ObservableCollection<ReportExportItem> ReportItems
         {
-            get { return this.reportItems; }
+            get 
+            {
+                WorkTypes[] workTypes = this.workTypesFilter.Where(w => w.IsSelected == true).Select(w => w.WorkType).ToArray();
+                List<ReportExportItem> list = this.reportList.Where(r => workTypes.Contains(r.WorkType) &&
+                    r.WeekOfYear == this.weekOfYearFilter).ToList();
+                this.reportItems = new ObservableCollection<ReportExportItem>(list);
+                return this.reportItems;
+            }
             set
             {
                 if (this.reportItems != value)
@@ -40,6 +107,23 @@ namespace PunchIn.ViewModels
                     this.reportItems = value;
                     OnPropertyChanged("ReportItems");
                 }
+            }
+        }
+        private ICommand checkedCommand;
+        public ICommand CheckedCommand
+        {
+            get
+            {
+                if (this.checkedCommand == null)
+                    this.checkedCommand = new DelegateCommand
+                    {
+                        CanExecuteFunc = (o) => true,
+                        CommandAction = (o) =>
+                            {
+                                OnPropertyChanged("ReportItems");
+                            }
+                    };
+                return this.checkedCommand;
             }
         }
         
@@ -56,5 +140,33 @@ namespace PunchIn.ViewModels
                 }
             }
         }
+        private string errors;
+        public string Errors
+        {
+            get { return this.errors; }
+            set
+            {
+                if (this.errors != value)
+                {
+                    this.errors = value;
+                    OnPropertyChanged("Errors");
+                }
+            }
+        }
+        #endregion
+    }
+
+    public class WorkTypesListCheckbox : ViewModelBase
+    {
+        public WorkTypes WorkType { get; set; }
+        public string Text { get; set; }
+        public ICommand Command { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+    public class WeekOfYearItem
+    {
+        public int WeekOfYear { get; set; }
+        public string WeekOfYearDate {get;set;}
     }
 }
