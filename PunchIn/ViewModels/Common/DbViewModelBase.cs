97e1c4907762434d1e9f4aeafdd33d4e7c813ca0
he62276 <vernon.laskey@health.wa.gov.au>
2014-10-17 17:51:04 +0800

    Add ViewModelBase specific for DB entities

---
+++ PunchIn/ViewModels/Common/DbViewModelBase.cs
@@ -0,0 +1,39 @@
+﻿using EmitMapper;
+using EmitMapper.MappingConfiguration;
+using System;
+using System.Collections.Generic;
+
+namespace PunchIn.ViewModels
+{
+    public abstract class DbViewModelBase : ViewModelBase
+    {
+        #region Methods
+        public static TViewModel ToViewModel<TModel, TViewModel>(TModel model)
+        {
+            return ObjectMapperManager.DefaultInstance.GetMapper<TModel, TViewModel>().Map(model);
+        }
+        public static TViewModel ToViewModel<TModel, TViewModel>(TModel model, ObjectsMapper<TModel, TViewModel> mapper)
+        {
+            return mapper.Map(model);
+        }
+        public TModel ToModel<TViewModel, TModel>(IGuidPK viewModel)
+        {
+            return ObjectMapperManager.DefaultInstance
+                .GetMapper<TViewModel, TModel>(
+                    new DefaultMapConfig().ConstructBy<TModel>(() => (TModel)Activator.CreateInstance(typeof(TModel), viewModel.Id))
+                ).Map((TViewModel)viewModel);
+        }
+
+        public static TViewModel ToViewModelWithList<TModel, TViewModel, TChildModel, TChildViewModel>(TModel model)
+        {
+            var mapper = EmitMapper.ObjectMapperManager.DefaultInstance
+                .GetMapper<TModel, TViewModel>(
+                    new EmitMapper.MappingConfiguration.DefaultMapConfig().ConvertGeneric(typeof(IList<TChildModel>), typeof(IList<TChildViewModel>),
+                        new EmitMapper.MappingConfiguration.DefaultCustomConverterProvider(typeof(Converters.ModelListToViewModelListConverter<TChildModel, TChildViewModel>))
+                    )
+                );
+            return mapper.Map(model);
+        }
+        #endregion
+    }
+}
